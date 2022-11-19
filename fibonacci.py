f513d869edb7e5c0c76aabe638326a93f97b8a93
Aryavarmakm <117915843+Aryavarmakm@users.noreply.github.com>
2022-11-19 23:26:45 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -8,3 +8,31 @@ num=int(input())
 print ("fibonacci series : ")
 for i in range(num):
     print(fibonacci(i))
+
+    
+    
+ OUTPUT :-
+Enter the number of terms
+20
+fibonacci series : 
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+55
+89
+144
+233
+377
+610
+987
+1597
+2584
+4181
+> 
